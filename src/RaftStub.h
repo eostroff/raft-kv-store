cffda3b9d0e0536f388e82cd13d671aa7124d5b4
Ethan Ostroff <eostroff@login-el8.khoury.northeastern.edu>
2026-04-05 09:38:19 -0400

    add RaftStub class for handling RPC communication with peers

---
+++ src/RaftStub.h
@@ -0,0 +1,22 @@
+#ifndef __CLIENT_STUB_H__
+#define __CLIENT_STUB_H__
+
+#include <string>
+#include "PeerClientSocket.h"
+#include "Messages.h"
+
+class RaftStub {
+private:
+	PeerClientSocket socket;
+public:
+	RaftStub();
+	int Init(std::string ip, int port);
+
+	// Send RPCs and receive replies
+	RequestVoteReply SendRequestVote(RequestVote req);
+	AppendEntriesReply SendAppendEntries(AppendEntries ae);
+
+	void Close();
+};
+
+#endif // end of #ifndef __CLIENT_STUB_H__

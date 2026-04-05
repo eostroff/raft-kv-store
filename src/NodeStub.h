6f4981efe3b90f5e7052f99b0c13e81f47cdcca7
Ethan Ostroff <eostroff@login-el8.khoury.northeastern.edu>
2026-04-05 09:38:02 -0400

    add NodeStub class for handling RPC message interactions (like a server stub)

---
+++ src/NodeStub.h
@@ -0,0 +1,38 @@
+#ifndef __NODE_STUB_H__
+#define __NODE_STUB_H__
+
+#include <memory>
+#include "ListenSocket.h"
+#include "Messages.h"
+
+class NodeStub {
+private:
+	std::unique_ptr<ListenSocket> socket;
+public:
+	NodeStub();
+	void Init(std::unique_ptr<ListenSocket> socket);
+
+	// Receive the RPC type tag (first 4 bytes) to determine message type
+	int ReceiveRPCType();
+
+	// Receive specific messages (call after peeking the type)
+	RequestVote ReceiveRequestVote();
+	AppendEntries ReceiveAppendEntries();
+
+	// Send replies
+	int SendRequestVoteReply(RequestVoteReply reply);
+	int SendAppendEntriesReply(AppendEntriesReply reply);
+
+	// Send requests (used by leader/candidate through peer connections)
+	int SendRequestVote(RequestVote req);
+	int SendAppendEntries(AppendEntries ae);
+
+	// Receive replies
+	RequestVoteReply ReceiveRequestVoteReply();
+	AppendEntriesReply ReceiveAppendEntriesReply();
+
+	bool IsValid();
+	void Close();
+};
+
+#endif // end of #ifndef __NODE_STUB_H__

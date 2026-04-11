3d04bed343885cd873d1926d0909fda776b45fc3
Ethan Ostroff <eostroff@login-el8.khoury.northeastern.edu>
2026-04-11 16:09:14 -0400

    implement LogManager class for managing log entries in Raft protocol

---
+++ src/LogManager.cpp
@@ -0,0 +1,75 @@
+#include "LogManager.h"
+
+LogManager::LogManager() {}
+
+int LogManager::LastIndex() const {
+	return (int)entries.size();
+}
+
+int LogManager::LastTerm() const {
+	if (entries.empty()) {
+		return 0;
+	}
+	return entries.back().term;
+}
+
+int LogManager::TermAt(int index) const {
+	if (index == 0) {
+		return 0;
+	}
+	if (index < 0 || index > (int)entries.size()) {
+		return -1;
+	}
+	return entries[index - 1].term;
+}
+
+bool LogManager::HasMatchingEntry(int index, int term) const {
+	if (index == 0) {
+		return term == 0;
+	}
+	if (index < 0 || index > (int)entries.size()) {
+		return false;
+	}
+	return entries[index - 1].term == term;
+}
+
+int LogManager::Append(int term, int command) {
+	entries.push_back({term, command});
+	return (int)entries.size();
+}
+
+std::vector<LogEntry> LogManager::EntriesFrom(int start_index) const {
+	std::vector<LogEntry> out;
+	if (start_index <= 0 || start_index > (int)entries.size() + 1) {
+		return out;
+	}
+	for (int i = start_index - 1; i < (int)entries.size(); ++i) {
+		out.push_back(entries[i]);
+	}
+	return out;
+}
+
+bool LogManager::AppendFromLeader(int prev_log_index, int prev_log_term,
+	const std::vector<LogEntry> &new_entries) {
+	if (!HasMatchingEntry(prev_log_index, prev_log_term)) {
+		return false;
+	}
+
+	int local_index = prev_log_index + 1;
+	int incoming_offset = 0;
+
+	while (incoming_offset < (int)new_entries.size() && local_index <= (int)entries.size()) {
+		if (entries[local_index - 1].term != new_entries[incoming_offset].term) {
+			entries.resize(local_index - 1);
+			break;
+		}
+		local_index++;
+		incoming_offset++;
+	}
+
+	for (int i = incoming_offset; i < (int)new_entries.size(); ++i) {
+		entries.push_back(new_entries[i]);
+	}
+
+	return true;
+}
---
+++ src/LogManager.h
@@ -0,0 +1,34 @@
+#ifndef __LOG_MANAGER_H__
+#define __LOG_MANAGER_H__
+
+#include <vector>
+
+struct LogEntry {
+	int term;
+	int command;
+};
+
+class LogManager {
+private:
+	std::vector<LogEntry> entries;
+
+public:
+	LogManager();
+
+	int LastIndex() const;
+	int LastTerm() const;
+	int TermAt(int index) const;
+	bool HasMatchingEntry(int index, int term) const;
+
+	int Append(int term, int command);
+	std::vector<LogEntry> EntriesFrom(int start_index) const;
+
+	// Raft conflict resolution:
+	// 1) validate prev_log_index/term
+	// 2) delete conflicting suffix
+	// 3) append new entries
+	bool AppendFromLeader(int prev_log_index, int prev_log_term,
+		const std::vector<LogEntry> &new_entries);
+};
+
+#endif // end of #ifndef __LOG_MANAGER_H__
